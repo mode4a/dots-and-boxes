6d7c037dab2b2f35781e12951b92f49da45f65dc
abdonaware <153302800+abdonaware@users.noreply.github.com>
2023-12-23 19:50:55 +0200

    adding beginner level and time

--- top_10.c
+++ top_10.c
@@ -7,7 +7,7 @@
 #include "top_10.h"
 top_10 winner;
 top_10 array[50];
-int x = 0;
+int number_of_players= 0;
 int comparePeople(const void *a, const void *b)
 {
     // Assuming you want to sort by score
@@ -17,8 +17,8 @@ void showing_champion()
 {
     FILE *champions = fopen("champions.bin", "rb+");
     fread(array, sizeof(top_10), 50, champions);
-    fread(&x,sizeof(int),1,champions);
-    qsort(array, x, sizeof(top_10), comparePeople);
+    fread(&number_of_players,sizeof(int),1,champions);
+    qsort(array, number_of_players, sizeof(top_10), comparePeople);
     printf("   name                    score  \n");
     for (int i = 0; i < 10; i++)
     {
@@ -29,38 +29,38 @@ void showing_champion()
 }
 void saving_champion(int high_score)
 {
-    FILE *champions = fopen("champions.bin", "wb+");
-    if (champions = NULL)
+    FILE *champions = fopen("champions.bin", "rb+");
+    if (!champions )
     {
         printf("error in opening file");
     }
     else
     {
         fread(array, sizeof(top_10), 50, champions);
-        fread(&x, sizeof(int), 1, champions);
+        fread(&number_of_players, sizeof(int), 1, champions);
         fclose(champions);
         winner.score = high_score;
-        int flag = 0, y;
-        for (int i = 0; i < x; i++)
+        int flag = 0, counter;
+        for (int i = 0; i < number_of_players; i++)
         {
             if (strcmp(array[i].name, winner.name) == 0)
             {
                 flag = 1;
-                y = i;
+                counter = i;
                 break;
             }
         }
         if (flag == 1)
         {
-            if (array[y].score > high_score)
+            if (array[counter].score > high_score)
             {
                 showing_champion();
             }
             else
             {
-                array[y].score = high_score;
+                array[counter].score = high_score;
                 FILE *old_champions = fopen("champions.bin", "wb+");
-                qsort(array, x, sizeof(top_10), comparePeople);
+                qsort(array, number_of_players, sizeof(top_10), comparePeople);
                 fwrite(array, sizeof(winner), 50, old_champions);
                 fclose(old_champions);
                 showing_champion();
@@ -68,17 +68,16 @@ void saving_champion(int high_score)
         }
         else
         {
-            array[x] = winner;
-            x++;
-            for (int i = x ; i < 11; i++)
+            array[number_of_players++] = winner;
+            for (int i = number_of_players ; i < 11; i++)
             {
                 strcpy(array[i].name, "no name");
                 array[i].score = 000;
             }
             FILE *old_champions = fopen("champions.bin", "wb+");
-            qsort(array, x, sizeof(top_10), comparePeople);
+            qsort(array, number_of_players, sizeof(top_10), comparePeople);
             fwrite(array, sizeof(winner), 50, old_champions);
-            fwrite(&x, sizeof(int), 1, old_champions);
+            fwrite(&number_of_players, sizeof(int), 1, old_champions);
             fclose(old_champions);
             showing_champion();
         }
